bca813a879438056b5b241669a1e6d3ec2b1a07e
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-07-29 23:51:05 +0200

    Add WIP: travel-list

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,10 @@
+export default function App() {}
+
+function Logo() {
+  return <h1>Far Away🌴💼</h1>;
+}
+function Form() {
+  return <div className="add-form"></div>;
+}
+function PackingList() {}
+function Stats() {}
