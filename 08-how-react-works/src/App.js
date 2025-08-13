5f2f8cdbe106ce9ae86ce17b8ed424f5977ae29f
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-08-13 22:41:31 +0200

    Add test

--- 08-how-react-works/src/App.js
+++ 08-how-react-works/src/App.js
@@ -26,6 +26,9 @@ export default function App() {
   );
 }
 
+// console.log(<DifferentContent />);
+// console.log(DifferentContent());
+
 function Tabbed({ content }) {
   const [activeTab, setActiveTab] = useState(0);
 
