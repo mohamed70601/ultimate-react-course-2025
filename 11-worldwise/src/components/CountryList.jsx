b1f3411771faf3a78e2e330104e7371b89d27438
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-09-07 19:58:45 +0200

    Fix map

--- 11-worldwise/src/components/CountryList.jsx
+++ 11-worldwise/src/components/CountryList.jsx
@@ -3,8 +3,10 @@ import CountryItem from "./CountryItem";
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import { useCities } from "../contexts/Cities.Context";
 
-function CountriesList({ cities, isLoading }) {
+function CountriesList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
