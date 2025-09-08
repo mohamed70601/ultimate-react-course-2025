1a780e134b3fde1e4a197eb6bd98cbb0ea7b2966
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-09-08 20:14:05 +0200

    App complete

--- 11-worldwise/src/contexts/CitiesContext.jsx
+++ 11-worldwise/src/contexts/CitiesContext.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useReducer } from "react";
+import { useCallback } from "react";
 import { useContext } from "react";
 import { createContext, useEffect } from "react";
 
@@ -81,20 +82,23 @@ function CitiesProvider({ children }) {
     fetchCities();
   }, []);
 
-  async function getCity(id) {
-    if (Number(id) === currentCity.id) return;
-    dispatch({ type: "loading" });
-    try {
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      dispatch({ type: "city/loaded", payload: data });
-    } catch {
-      dispatch({
-        type: "rejected",
-        payload: "There was an error loading city.",
-      });
-    }
-  }
+  const getCity = useCallback(
+    async function getCity(id) {
+      if (Number(id) === currentCity.id) return;
+      dispatch({ type: "loading" });
+      try {
+        const res = await fetch(`${BASE_URL}/cities/${id}`);
+        const data = await res.json();
+        dispatch({ type: "city/loaded", payload: data });
+      } catch {
+        dispatch({
+          type: "rejected",
+          payload: "There was an error loading city.",
+        });
+      }
+    },
+    [currentCity.id]
+  );
 
   async function createCity(newCity) {
     dispatch({ type: "loading" });
