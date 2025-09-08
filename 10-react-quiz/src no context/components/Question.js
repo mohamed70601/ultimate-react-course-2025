d787cf64ece7125e8b2276acc46815e5c18704ba
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-09-08 13:35:16 +0200

    Refactored to context

--- 10-react-quiz/src no context/components/Question.js
+++ 10-react-quiz/src no context/components/Question.js
@@ -1,9 +1,6 @@
-import { useQuiz } from "../contexts/QuizContext";
 import Options from "./Options";
 
-function Question() {
-  const { questions, dispatch, answer, index } = useQuiz();
-  const question = questions.at(index);
+function Question({ question, dispatch, answer }) {
   return (
     <div>
       <h4>{question.question}</h4>
