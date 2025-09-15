0efb4fcd051b3ed89b3900bf1fb3fa96beda6467
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-09-15 23:27:38 +0200

    Add RTK

--- 15-fast-react-pizza/src/features/cart/Cart.jsx
+++ 15-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
@@ -38,7 +37,7 @@ function Cart() {
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
-          <CartItem item={item} key={item.key} />
+          <CartItem item={item} key={item.pizzaId} />
         ))}
       </ul>
 
