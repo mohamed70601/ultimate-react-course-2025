8ef65aecc183c984ff43d4159621add24ed8e2c3
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-09-24 19:23:30 +0200

    Build bookings table

--- 16-the-wild-oasis/src/features/cabins/CabinTable.jsx
+++ 16-the-wild-oasis/src/features/cabins/CabinTable.jsx
@@ -1,15 +1,18 @@
+import { useCabins } from "./useCabins";
+import { useSearchParams } from "react-router-dom";
+
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
-import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
   const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+  if (!cabins.length) return <Empty resourceName="cabins" />;
 
   // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
