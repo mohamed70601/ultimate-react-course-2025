dc151fb119fcd21b9efd282097713e7538064324
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-09-24 20:51:21 +0200

    Implement API-side filter & sort

--- 16-the-wild-oasis/src/features/bookings/useBookings.js
+++ 16-the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,14 +1,29 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParms] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParms.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
+  // SORT
+  const sortByRaw = searchParms.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, error, bookings };
