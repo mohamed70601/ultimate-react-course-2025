8af5b007f81b4bd17c7ecfa97aca29f8780e1cab
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-09-25 23:06:22 +0200

    Add WIP#1: chart

--- 16-the-wild-oasis/src/features/dashboard/SalesChart.jsx
+++ 16-the-wild-oasis/src/features/dashboard/SalesChart.jsx
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 import DashboardBox from "./DashboardBox";
+import Heading from "../../ui/Heading";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 const StyledSalesChart = styled(DashboardBox)`
   grid-column: 1 / -1;
@@ -57,3 +67,18 @@ const colors = isDarkMode
       text: "#374151",
       background: "#fff",
     };
+
+function SalesChart() {
+  return (
+    <StyledSalesChart>
+      <Heading as="h2">Sales</Heading>
+
+      <AreaChart data={fakeData} height={300} width={700}>
+        <CartesianGrid />
+        <Area dataKey="totalSales" type="monotone" stroke="red" fill="orange" />
+      </AreaChart>
+    </StyledSalesChart>
+  );
+}
+
+export default SalesChart;
