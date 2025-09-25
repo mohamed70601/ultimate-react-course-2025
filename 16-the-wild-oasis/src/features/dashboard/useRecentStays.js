bca25e7415ab7d556ecb6b51c7a489c4edcf946c
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2025-09-25 22:54:27 +0200

    Display statistics

--- 16-the-wild-oasis/src/features/dashboard/useRecentStays.js
+++ 16-the-wild-oasis/src/features/dashboard/useRecentStays.js
@@ -20,5 +20,5 @@ export function useRecentStays() {
     (stay) => stay.status === "checked-in" || "checked-out"
   );
 
-  return { isLoading, stays, confirmedStays };
+  return { isLoading, stays, confirmedStays, numDays };
 }
